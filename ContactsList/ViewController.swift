e0e5f05aa1055ec7e5c60f8b78f759062d6a379a
Karen Tsarukyan <tsarukyan2016@icloud.com>
2022-04-03 22:00:38 +0500

    Initial Commit

---
+++ ContactsList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsList
+//
+//  Created by Karen Tsarukian on 03.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
